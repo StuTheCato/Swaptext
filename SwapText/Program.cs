1a11b3d1a1f353d83963a27dcb0ba27130bcbf36
StuTheCato <stuat.@Stuats-MacBook-Air.local>
2024-02-10 18:50:03 +0100

    Initial commit

---
+++ SwapText/Program.cs
@@ -0,0 +1,21 @@
+﻿using SwapText;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Txt txt = new Txt();
+
+        Console.Write("Type your first word: ");
+        string wordOne = Console.ReadLine();
+        
+        Console.Write("Type your second word: ");
+        string wordTwo = Console.ReadLine();
+        
+        Console.WriteLine($"Before the swap: {wordOne} {wordTwo}");
+        
+        txt.Swaptext(ref wordOne, ref wordTwo);
+        
+        Console.WriteLine($"After the swap: {wordOne} {wordTwo}");
+    }
+}
---
+++ SwapText/Txt.cs
@@ -0,0 +1,12 @@
+namespace SwapText;
+
+public class Txt
+{
+    public void Swaptext(ref string x, ref string y)
+    {
+        
+        string temp = x;
+        x = y;
+        y = temp;
+    }
+}
